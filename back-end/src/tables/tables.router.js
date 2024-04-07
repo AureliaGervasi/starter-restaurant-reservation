4db8fe9a2b76caa62b3dc972ba1d676cdad409ed
Aurelia <aurelia.gervasi@gmail.com>
2024-04-07 16:18:16 -0400

    edit

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const controller = require("./tables/controller");
+const controller = require("./tables.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/:table_id/seat")
