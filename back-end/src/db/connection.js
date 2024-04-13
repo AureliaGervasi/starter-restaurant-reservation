c687ba1678611512950d4a46614823dd736d4d50
Aurelia <aurelia.gervasi@gmail.com>
2024-04-13 00:47:19 -0400

    update

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,3 +1,7 @@
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+
+
 const environment = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,6 +1,3 @@
-const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
-
 const { PORT = 5001 } = process.env;
 
 const app = require("./app");
